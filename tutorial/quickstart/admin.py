de45afa7e7a2f8e49ed0a1555924dc9c370457b1
dharmendra <dvishwakarma85241@gmail.com>
2022-02-28 20:37:04 +0530

    history test

--- tutorial/quickstart/admin.py
+++ tutorial/quickstart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from simple_history import register
+from django.contrib.auth.models import User
+
+register(User)
--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from simple_history.models import HistoricalRecords
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    history = HistoricalRecords()
